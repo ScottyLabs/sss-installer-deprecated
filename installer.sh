ab278258e52d0e4bbf6ebb4223b389005c7b7364
Yuxiang-Huang <yuxiangh142857@gmail.com>
2026-01-07 05:44:13 -0500

    feat: clone repo with sparse checkout to install scripts
    Results in folder structure of scripts/secrets/scripts/secrets, which is not ideal

--- installer.sh
+++ installer.sh
@@ -1,1 +1,18 @@
-echo "Hello World"
+#!/usr/bin/env bash
+# Licensed under the MIT license <LICENSE-MIT or https://opensource.org/licenses/MIT>,
+# and the Apache License 2.0 <LICENSE-APACHE or https://www.apache.org/licenses/LICENSE-2.0>,
+# at your option. This file may not be copied, modified, or distributed
+# except according to those terms.
+set -e
+
+# Create the scripts directory if it doesn't exist
+mkdir -p scripts
+cd scripts
+
+# Clone the remote scripts folder on GitHub using sparse checkout if
+git init
+git remote add origin https://github.com/ScottyLabs/sss-installer.git
+git config core.sparseCheckout true
+echo "scripts/secrets/*" >.git/info/sparse-checkout
+git fetch --depth=1 origin main
+git checkout main
